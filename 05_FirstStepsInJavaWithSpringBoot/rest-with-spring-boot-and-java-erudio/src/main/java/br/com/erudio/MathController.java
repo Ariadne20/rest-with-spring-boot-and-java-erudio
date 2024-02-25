905ff967777448145759e20cb75207704250a58b
Ariadne20 <ariliguima@gmail.com>
2024-02-25 19:50:42 -0300

    tratando exceptions

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.erudio.exceptions.UnsupportedMathOperationException;
+
 @RestController
 public class MathController {
 	
@@ -14,7 +16,7 @@ public class MathController {
 			@PathVariable(value="numberTwo") String numberTwo
 			) throws Exception {
 		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new Exception();
+			throw new UnsupportedMathOperationException("Please set a numeric value");
 		}
 		return covertToDouble(numberOne) + covertToDouble(numberTwo);
 	}
